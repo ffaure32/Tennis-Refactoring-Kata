8a774743ef84a887a2b0fba21f8c42f4139a209c
ffaure <frederic.faure@ext.cdiscount.com>
2019-01-14 13:25:25 +0100

    extract methods game1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,6 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -20,57 +20,67 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        int tempScore = 0;
+        if (equality()) {
+            score = scoreForEquality();
+        } else if (advantage()) {
+            score = scoreForAdvantage();
+        } else {
+            score = standardCode(score);
         }
         return score;
+    }
+
+    private final String[] SCORES_GAME = {"Love", "Fifteen", "Thirty", "Forty"};
+    private String standardCode(String score) {
+        int tempScore;
+
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = m_score1;
+            else {
+                score += "-";
+                tempScore = m_score2;
+            }
+            score += SCORES_GAME[tempScore];
+        }
+        return score;
+    }
+
+    private String scoreForAdvantage() {
+        String score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
+
+    private boolean advantage() {
+        return m_score1 >= 4 || m_score2 >= 4;
+    }
+
+    private String scoreForEquality() {
+        String score;
+        switch (m_score1) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+
+    private boolean equality() {
+        return m_score1 == m_score2;
     }
 }
