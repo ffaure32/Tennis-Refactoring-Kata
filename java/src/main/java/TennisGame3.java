a63b2bd704db457a34e7a7f1762c091e4ba3535f
ffaure <frederic.faure@ext.cdiscount.com>
2019-01-15 13:17:55 +0100

    refacto TennisGame3

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,6 +1,8 @@
 
 public class TennisGame3 implements TennisGame {
-    
+
+    private static final String[] SCORES = {"Love", "Fifteen", "Thirty", "Forty"};
+    public static final String SCORE_SEPARATOR = "-";
     private int p2;
     private int p1;
     private String p1N;
@@ -12,25 +14,58 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        if (isEquality()) {
+            return scoreForEquality();
+        }
+        if (isStandardScore()) {
+            return scoreForStandard();
         } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            return scoreForAdvantageOrWin();
         }
     }
-    
+
+    private String scoreForAdvantageOrWin() {
+        return getAdvantageOrWin() + getLeader();
+    }
+
+    private String scoreForStandard() {
+        return SCORES[p1] + "-" + SCORES[p2];
+    }
+
+    private boolean isStandardScore() {
+        return p1 < 4 && p2 < 4;
+    }
+
+    private String getAdvantageOrWin() {
+        return isAdvantage() ? "Advantage " : "Win for ";
+    }
+
+    private String getLeader() {
+        return p1 > p2 ? p1N : p2N;
+    }
+
+    private boolean isAdvantage() {
+        return Math.abs(p1 - p2) == 1;
+    }
+
+    private String scoreForEquality() {
+        if (p1 >= 3) {
+            return "Deuce";
+        } else {
+            return SCORES[p1] + SCORE_SEPARATOR + "All";
+        }
+    }
+
+    private boolean isEquality() {
+        return p1 == p2;
+    }
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
             this.p1 += 1;
         else
             this.p2 += 1;
-        
+
     }
 
 }
